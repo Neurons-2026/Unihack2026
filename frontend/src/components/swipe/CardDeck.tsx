a50da5be3deb0d0750cfbf82c507ad1c82b08a48
Steve <yimizheng@student.unimelb.edu.au>
2026-03-15 16:26:21 +1100

    changed bottom value

--- frontend/src/components/swipe/CardDeck.tsx
+++ frontend/src/components/swipe/CardDeck.tsx
@@ -293,7 +293,7 @@ export default function CardDeck({ activeTopic }: { activeTopic: string }) {
         <div
           style={{
             position: 'absolute',
-            bottom: 100,
+            bottom: 250,
             left: '50%',
             transform: 'translateX(-50%)',
             zIndex: 20,
